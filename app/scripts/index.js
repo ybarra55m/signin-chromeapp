468bc877dcaf4372dcd6dc0226749c100154b70e
rigel <me@wrinklynose>
2014-11-12 20:31:55 -0800

    working on wiring up jquery backend

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,17 @@
 'use strict';
-document.addEventListener('DOMContentLoaded', function() {
-}, false);
+    $(document).ready(function ($) {
+        var record = [];
+        $('#submitFormButton').click(function () {
+            record.push({
+                sent: Date.now(),
+                checked: $("input:checked").val(),
+            });
+            console.log(record);
+            $.ajax({
+                type: "POST",
+                url: "send",
+                data: JSON.stringify(record)
+            });
+        });
+    });
+
