2d930416653a6a3fc085d551f35801ac23a278e5
rigel <me@wrinklynose>
2014-11-14 23:34:33 -0800

    patron record is an object now, with timestamp (UNIX epoch, because that's easy). still TODO: clear info, show 'signed in' alert, ask for demographics, log to back-end (currently going to console.log).

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,13 @@
 $('button:submit').click(function (event) {
     event.preventDefault();
+    var record = {};
+    record['datetime'] = Date.now();
     $(':input').each(function () {
         if ((this.checked) || ($(this).attr('type') === 'text')) {
-            console.log($(this).val());
+            var i = $(this);
+            record[i.attr('name')] = i.val();
         };
     });
+    console.log(record);
 });
 
